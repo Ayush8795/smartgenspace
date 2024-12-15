4990b75cdfb408f1ee2114956e4abb286e448044
Ayush8795 <ayumanz8795@gmail.com>
2024-12-15 19:34:12 +0530

    Updates

---
+++ precache-manifest.4299a33cd23b305aa3f1c93e51acf2eb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c11b2f820f6602e483b17f29b3b1dad1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6fedd4d336fc34c9daa2",
+    "url": "/static/css/main.a9bca2ef.chunk.css"
+  },
+  {
+    "revision": "962b6074b57f0b747e69",
+    "url": "/static/js/2.d43adcb1.chunk.js"
+  },
+  {
+    "revision": "20f46a6d5c56b6bc85c4a5647ee397c4",
+    "url": "/static/js/2.d43adcb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fedd4d336fc34c9daa2",
+    "url": "/static/js/main.3f46ffb2.chunk.js"
+  },
+  {
+    "revision": "a21e936436e8a5db4b79",
+    "url": "/static/js/runtime-main.040bf918.js"
+  },
+  {
+    "revision": "f76dc7d6a05aa59a19db988927cab9d6",
+    "url": "/static/media/Logo.f76dc7d6.jpeg"
+  },
+  {
+    "revision": "1baa13aa7584a12d40f2940d0623cfa5",
+    "url": "/static/media/background.1baa13aa.jpg"
+  }
+]);
